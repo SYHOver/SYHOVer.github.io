0cdb7459c1664c81da014dee0d8f69e43de3c536
SYHOver <834725611@qq.com>
2021-11-15 10:22:28 +0800

    提交

--- webpack.config.js
+++ webpack.config.js
@@ -89,3 +89,134 @@ module.exports = async () => {
   }
   return config;
 };
+
+const fs = require("fs");
+class FileSystem {
+    constructor(params) {
+        this.baseDir = params.baseDir;
+    }
+
+    /**
+      * @description 该方法是用来写文件的
+      * @param fileName <arr>  文件的名字
+      */
+    writeFiles(fileName) {
+            fs.writeFile(fileName,fileName, function () {})
+    }
+
+
+    /**
+    * @description  该方法是用来得到文件格式的
+    * @param filename <string> 文件名称
+    * @returns <arr> 文件格式
+    * **/
+    formatFile(fileName) {
+        return [
+            {
+                name: "./src/" + fileName + "/" + fileName + ".js",
+                content: fileName + ".js"
+            },
+            {
+                name: "./src/" + fileName + "/" + fileName + ".html",
+                content: fileName + ".html"
+            },
+            {
+                name: "./src/" + fileName + "/" + fileName + ".less",
+                content: fileName + ".less"
+            }
+        ]
+    }
+
+
+    /**
+ * @description  如果有该文件返回true
+ * @param filename,  <string> 文件名称  
+ * @param type,      <number> 是文件还是文件夹
+ * @returns <Boolean>
+ * **/
+    //
+    getStat(fileName, type) {
+        return new Promise(function (resolve) {
+            var exists = false;  //默认false  文件不存在
+            fs.access(fileName, function (err) {
+                if (!err) {
+                    exists = true;    //先判断是否有这个文件或文件价  如果有再进行判断
+                }
+                if (exists) {
+                    fs.stat(fileName, function (err, data) {
+                        //type =1 判断是不是文件夹   2判断是不是文件
+                        if (!err) {
+                            type == 1 ? exists = data.isDirectory() : exists = data.isFile();
+                            resolve(exists);
+                        }
+                    })
+                } else {
+                    resolve(exists);
+                }
+            })
+        })
+    }
+
+    readdirFiles(templateUrl) {
+        let _this = this;
+        return new Promise(function (resolve, reject) {
+            fs.readdir(_this.baseDir + "/" + templateUrl, function (err, data) {
+                if (err) {
+                    reject(err);
+                } else {
+                    resolve(data);
+                }
+            })
+        })
+    }
+
+    /**
+     * @description  该方法是用来复制文件的
+     * @param filename <string> 文件名称
+     * @param templateUrl <string> 复制模板路径
+     * **/
+    async copyFiles(fileName, templateUrl) {
+        let src = "./src/";
+        let data = await this.readdirFiles(templateUrl);
+        for (let i = 0; i < data.length; i++) {
+            let tailArr = data[i].split(".");
+            fs.copyFile(src + templateUrl + "/" + data[i], src + fileName + "/" + fileName + "." + tailArr[tailArr.length - 1], function () { })
+        }
+    }
+
+    mkdir(fileName) {
+        let str = "/";
+        fs.mkdir(this.baseDir + str + fileName, async function(){})
+    }
+
+    generateModules(fileObj) {
+        let str = "/";
+        var _this = this;
+        fileObj.modules.forEach(async (fileName, index) => {
+            let res = await _this.getStat(_this.baseDir + str + fileName, 1);
+            if (res) {
+                let res = await _this.getStat(_this.formatFile(fileName)[index].name,2);
+                if (res && fileObj.replace) {
+                    _this.copyFiles(fileName, fileObj.template);
+                } else {
+                    _this.writeFiles(_this.formatFile(fileName)[index].name);
+                }
+            } else {
+                _this.mkdir(fileName);
+                _this.copyFiles(fileName, fileObj.template);
+            }
+        })
+    }
+}
+
+let fileSystem = new FileSystem({
+    baseDir: './src'
+})
+
+var modules = ['home', 'article', 'articleDetail'];
+
+fileSystem.generateModules({
+    modules,   //模块名的集合，必填项
+    template: 'template',   //创建模块所依赖的模板， 非必填项，如不填，默认指的是 this.baseDir + '/' + template 文件夹；
+    replace: false //非必填项， 默认为false，如果是false，在创建模板时，不会替换掉之前已存在的文件内容， ture是强制替换现有的模板文件及文件内容；
+});
