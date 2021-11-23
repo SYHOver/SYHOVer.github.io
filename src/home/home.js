65c6b7731b65519bd25e778c014dc8fd7865c4c7
SYHOver <834725611@qq.com>
2021-11-23 08:11:51 +0800

    接口文档

--- src/home/home.js
+++ src/home/home.js
@@ -1,1 +1,17 @@
-./src/home/home.js
+let btns = document.querySelectorAll("li");
+
+
+// let btns = document.querySelector(".article_left");
+// console.log(btns.childNodes[1].children);
+
+
+for (let i = 0; i < btns.length; i++) {
+    btns[i].onclick = function () {
+        for (let j = 0; j < btns.length; j++) {
+            btns[j].className = ""
+        }
+        this.className = "reed"
+    }
+}
+
+
